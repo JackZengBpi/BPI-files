a93660f52e2da651bd59d9f756102352493f11a3
Banana Pi -BPI <lionwang@sinovoip.com.cn>
2016-11-23 17:28:35 +0800

    bpi-service: update audio script

--- others/bpi-service/var/lib/bananapi/bpi-autorun.d/S10-audio-bpi-m2u.sh
+++ others/bpi-service/var/lib/bananapi/bpi-autorun.d/S10-audio-bpi-m2u.sh
@@ -3,32 +3,35 @@ BOARD=$(bpi-hw)
 
 ASOUNDFILE=/var/lib/alsa/asound.state.$BOARD
 
-if [ "x$BOARD" != "xbpi-m2u" ] ; then
-	return 0
-fi
+audio-bpi-m2u() {
+  if [ "x$BOARD" != "xbpi-m2u" ] ; then
+    return 0
+  fi
 
-if [ ! -x "/usr/bin/amixer" ] ; then
-	return 0
-fi
+  if [ ! -x "/usr/bin/amixer" ] ; then
+    return 0
+  fi
 
-# BPI-M2U do audio setup
-if [ -x "/usr/sbin/alsactl" ] ; then
-	if [ -f $ASOUNDFILE ] ; then
-		/usr/sbin/alsactl -f $ASOUNDFILE restore 2>&1 >/dev/null
-	fi
-fi
+  # BPI-M2U do audio setup
+  if [ -x "/usr/sbin/alsactl" ] ; then
+    if [ -f $ASOUNDFILE ] ; then
+      /usr/sbin/alsactl -f $ASOUNDFILE restore 2>&1 >/dev/null
+    fi
+  fi
 
+  # amic mixer
+  /usr/bin/amixer -c 0 cset numid=25,iface=MIXER,name='Right Input Mixer MIC1 Boost Switch' 1 2>&1 >/dev/null
 
-# amic mixer
-/usr/bin/amixer -c 0 cset numid=25,iface=MIXER,name='Right Input Mixer MIC1 Boost Switch' 1 2>&1 >/dev/null
+  # display 
+  #/usr/bin/amixer contents
 
-# display 
-#/usr/bin/amixer contents
+  # test mic
+  #arecord -fcd | aplay
 
-# test mic
-#arecord -fcd | aplay
+  if [ -x "/usr/sbin/alsactl" ] ; then
+    /usr/sbin/alsactl -f $ASOUNDFILE store 2>&1 >/dev/null
+  fi
+}
 
-if [ -x "/usr/sbin/alsactl" ] ; then
-	/usr/sbin/alsactl -f $ASOUNDFILE store 2>&1 >/dev/null
-fi
-
+#main
+audio-bpi-m2u
