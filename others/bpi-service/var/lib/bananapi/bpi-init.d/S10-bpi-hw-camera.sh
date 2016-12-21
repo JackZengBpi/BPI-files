bbbc2db6b1afad9d04c01969ac10cdf438f35e2b
Banana Pi -BPI <lionwang@sinovoip.com.cn>
2016-12-21 18:27:56 +0800

    bpi-service.tgz: add S10-bpi-hw-camera.sh support camera

---
+++ others/bpi-service/var/lib/bananapi/bpi-init.d/S10-bpi-hw-camera.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+BOARD=$(bpi-hw)
+
+load_modules()
+{
+  case ${BOARD} in
+  bpi-m3)
+#    MODULES="videobuf-core videobuf-dma-contig cci actuator vm149c_act vfe_os vfe_subdev ov8858_4lane vfe_v4l2"
+#    MODULES="vm149c_act ov8858_4lane vfe_v4l2"
+    MODULES="ov5640 vfe_v4l2"
+    ;;
+  bpi-m2u)
+    MODULES="ov5640 vfe_v4l2"
+    ;;
+  bpi-m2p)
+    MODULES="ov5640 vfe_v4l2"
+    ;;
+  bpi-m2)
+    MODULES="ov5640 vfe_v4l2"
+    ;;
+  *)
+    MODULES="ov5640 vfe_v4l2"
+    ;;
+  esac
+  for IN in $MODULES ; do
+    modprobe $IN
+  done
+}
+
+#main
+load_modules
